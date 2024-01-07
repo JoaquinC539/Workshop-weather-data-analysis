a13b55e56fb9151baff05d9247f67edfbb542056
JC <cjoaquinangel@gmail.com>
2024-01-07 14:13:56 -0600

    added machine selection

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,10 +1,12 @@
-import random
+from random import randint
 
 option = {
-    'rock': False,
+    'rock': tuple(False,1),
     'paper': False,
     'scissors': False
 }
+
+
 
 # Rock = False
 # Paper = False
@@ -18,8 +20,16 @@ while True:
         pass
 
 while True:
+    
     option = input('Please select an option... "Rock", "Paper" or "Scissors"...')
     option = option.lower()
+    # Seleccionar que la maquina tome un numero
+    selection=randint(1,3)
+    print(selection)
+    options=["rock","paper","scissors"]
+    machine=options[selection]
+    print(machine)
+    
     if option == 'rock' or option == 'r':
         option['rock'][True]
         break
