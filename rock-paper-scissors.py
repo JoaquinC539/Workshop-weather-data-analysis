b5080f43ef5301ddd76b63d2e6679eeea0cd5ec3
Tyrost <ccorzo306@gmail.com>
2024-01-07 11:49:27 -0800

    code

---
+++ rock-paper-scissors.py
@@ -0,0 +1,15 @@
+option = {
+    'Rock': False,
+    'Paper': False,
+    'Scissors': False
+}
+
+# Rock = False
+# Paper = False
+# Scissors = False
+
+while True:
+    input('Welcome to rock, paper, scissors game. Please hit "Return" to continue...')
+
+if 
+while True:
